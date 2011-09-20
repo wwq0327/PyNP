0483f71143047ff8550b4077273c7501467a5b71
wwq0327 <wwq0327@gmail.com>
2011-09-20 20:35:47 +0800

    第三个版本的连接，获取服务器信息

---
+++ codes/ch2/connect3.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    connect2.py
+    ~~~~~~~~~~~~~~~~~~~~
+    
+    :date: 2011-09-20
+    :from: Python Network Programming
+"""
+
+import socket
+
+print 'Creating socket...',
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'done.'
+
+print "Looking up port number...",
+port = socket.getservbyname('http', 'tcp')
+print 'done.'
+
+print "Connecting to remote host on port %d..." % port,
+s.connect(("www.google.com", port))
+print "done."
+
+print "Connected from", s.getsockname() ## 有意思的是，多次执行这个程序，端口会发生变化。
+print "Connected to", s.getpeername()  ## 服务器IP
